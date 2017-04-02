d454d83f1af6bed45984a994927244a9fd01cf72
archiebye <archiebye@gmail.com>
2017-04-02 09:45:30 +0100

    fix

--- accounts/models.py
+++ accounts/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     first_name = models.CharField(max_length=10, blank=True)
     last_name = models.CharField(max_length=15, blank=True)
     bio = models.TextField(max_length=500, blank=True)
-    school = models.CharField(max_length=30, choices=SCHOOL_CHOICES, default='1')
+    school = models.CharField(max_length=30, choices=SCHOOL_CHOICES, default='Architecture')
     birth_date = models.DateField(null=True, blank=True, default=date.today)
     gender = models.CharField(max_length=2, choices=GENDER_CHOICES, default='1')
 
